4f8ace1fb3c06d3d6c89a47bb3f6d21f49e3f624
Nshan02 <nshan.simonyan@list.ru>
2023-05-13 16:08:30 +0400

    new_file.py modified

--- new_file.py
+++ new_file.py
@@ -1,3 +1,14 @@
 def some():
     
     return 1
+
+class car:
+    
+    def __init__(self,name,model):
+        self.name = name
+        self.model = model
+        
+    @staticmethod    
+    def about():
+        print("THE CAR CLASS!")
+        
